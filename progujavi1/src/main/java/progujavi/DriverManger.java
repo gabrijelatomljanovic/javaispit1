af8533646769484166ae472e4a50fbb3b89629f3
gabrijelatomljanovic <57711762+gabrijelatomljanovic@users.noreply.github.com>
2022-06-30 20:12:23 +0200

    Add files via upload

---
+++ progujavi1/src/main/java/progujavi/DriverManger.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package progujavi;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author DT User6
+ */
+class DriverManger {
+
+    static Connection getConnection(String jdbcmysqllocalhostucenici, String gabi, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
